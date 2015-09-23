14f7337c4643a3245d01d4fd98c6efdf723f22fb
matsair <matsschade@gmail.com>
2015-09-23 10:30:00 +0200

    updated python scripts and classifier

--- src/Classifier.java
+++ src/Classifier.java
@@ -2,7 +2,9 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 
 import weka.classifiers.Evaluation;
+import weka.classifiers.bayes.NaiveBayes;
 import weka.classifiers.lazy.IBk;
+import weka.classifiers.trees.J48;
 import weka.core.Instances;
 import weka.core.SerializationHelper;
 
@@ -14,8 +16,8 @@ import weka.core.SerializationHelper;
 */
 public class Classifier {
 	
-	private static final String TRAIN_FILE = "assets/Reduced50HzSubject1Day0.arff";
-	private static final String MODEL_FILE = "assets/knnSubject1Day0.model";	
+	private static final String TRAIN_FILE = "assets/subject4allSamplewoUnknown.arff";
+	private static final String MODEL_FILE = "assets/subject4allSamplewoUnknown.J48";	
 	
 	public static void main(String[] args) throws Exception {
 		
@@ -27,18 +29,32 @@ public class Classifier {
 		// Set class attribute, which is the last attribute
 		data.setClassIndex(data.numAttributes() - 1);
 		
+		/*
 		// Declare and build the kNN classifier IBk(x), with x as k
 	    IBk classifier = new IBk(3);
 		classifier.buildClassifier(data);
+		*/
+		
+		// Declare and build the Decision Tree classifier J48()
+	    J48 classifier = new J48();
+		classifier.buildClassifier(data);
+		
+		/*
+		// Declare and build the Naive Bayes classifier
+	    NaiveBayes classifier = new NaiveBayes();
+		classifier.buildClassifier(data);
+		*/
 		
 		// Save classifier to file
 		SerializationHelper.write(MODEL_FILE, classifier);
 		System.out.println("Model written to file.");
+		
 		
 		// Evaluation of classifier
 		Evaluation eTest = new Evaluation(data);
 		eTest.evaluateModel(classifier, data);
 		System.out.println(classifier);
 		System.out.println(eTest.toSummaryString());
+		
 	}   
 }
